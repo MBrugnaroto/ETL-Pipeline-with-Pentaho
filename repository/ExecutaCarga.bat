883edc0a2b5f9e1a3fc58ff4183c93f026d7313a
MBrugnaroto <brugnaroto@gmail.com>
2021-12-29 21:41:43 -0300

    FEAT: Created .bat

---
+++ repository/ExecutaCarga.bat
@@ -0,0 +1,4 @@
+d:
+cd\
+cd D:/softwares/designer-tools/data-integration
+kitchen.bat /file:D:/projetos/BI-DW-with-Pentaho/repository/jobMain.kjb /level:Basic /log:"D:/projetos/BI-DW-with-Pentaho/logs/jobLog.log" > "D:/projetos/BI-DW-with-Pentaho/logs/consoleLog.log"
